5d5d6bb76781fc9eb5677c0b1343e91a755e4ae3
Nguyen Duc Hieu <hieunguyenduc696@gmail.com>
2022-01-20 23:49:32 +0700

    getting user input & handling form submission

--- src/components/meetups/NewMeetupForm.js
+++ src/components/meetups/NewMeetupForm.js
@@ -1,28 +1,57 @@
+import { useRef } from "react";
+
 import Card from "../ui/Card";
 import styles from "./NewMeetupForm.module.css";
 
 const NewMeetupForm = () => {
+  const titleInputRef = useRef();
+  const imageInputRef = useRef();
+  const addressInputRef = useRef();
+  const descriptionInputRef = useRef();
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+
+    const enteredTitle = titleInputRef.current.value;
+    const enteredImage = imageInputRef.current.value;
+    const enteredAddress = addressInputRef.current.value;
+    const enteredDescription = descriptionInputRef.current.value;
+
+    const newMeetup = {
+      enteredTitle,
+      enteredImage,
+      enteredAddress,
+      enteredDescription,
+    };
+
+    console.log(newMeetup);
+  };
   return (
     <Card>
-      <form className={styles.form}>
+      <form className={styles.form} onSubmit={submitHandler}>
         <div className={styles["form-control"]}>
           <label htmlFor="title">Meetup Title</label>
-          <input type="text" id="title" required />
+          <input type="text" id="title" required ref={titleInputRef} />
         </div>
         <div className={styles["form-control"]}>
           <label htmlFor="image">Meetup Image</label>
-          <input type="url" id="image" required />
+          <input type="url" id="image" required ref={imageInputRef} />
         </div>
         <div className={styles["form-control"]}>
           <label htmlFor="address">Meetup Address</label>
-          <input type="text" id="address" required />
+          <input type="text" id="address" required ref={addressInputRef} />
         </div>
         <div className={styles["form-control"]}>
           <label htmlFor="description">Meetup Description</label>
-          <textarea id="description" required rows={5} />
+          <textarea
+            id="description"
+            required
+            rows={5}
+            ref={descriptionInputRef}
+          />
         </div>
         <div className={styles.action}>
-            <button>Add Meetup</button>
+          <button>Add Meetup</button>
         </div>
       </form>
     </Card>
