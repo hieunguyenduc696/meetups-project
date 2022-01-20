0d263ec6dc7d0b2bc1f36d4a14d5c9f0d7d0a679
Nguyen Duc Hieu <hieunguyenduc696@gmail.com>
2022-01-21 00:09:40 +0700

    programmatically navigate

--- src/pages/NewMeetup.js
+++ src/pages/NewMeetup.js
@@ -1,13 +1,21 @@
+import { useHistory } from "react-router-dom";
+
 import NewMeetupForm from "../components/meetups/NewMeetupForm";
 
 const NewMeetupPage = () => {
+  const history = useHistory();
   const addMeetupHandler = (newMeetup) => {
-    fetch("https://react-project-a9a58-default-rtdb.firebaseio.com/meetups.json", {
-      method: "POST",
-      body: JSON.stringify(newMeetup),
-      headers: {
-        "Content-Type": "application/json",
-      },
+    fetch(
+      "https://react-project-a9a58-default-rtdb.firebaseio.com/meetups.json",
+      {
+        method: "POST",
+        body: JSON.stringify(newMeetup),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    ).then(() => {
+      history.replace("/");
     });
   };
   return (
