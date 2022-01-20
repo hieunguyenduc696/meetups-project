14460428536f8539fc8254eaea2a6e71eca47d82
Nguyen Duc Hieu <hieunguyenduc696@gmail.com>
2022-01-20 17:30:27 +0700

    adding links & navigation

--- src/App.js
+++ src/App.js
@@ -1,21 +1,26 @@
+import React from "react";
 import { Route, Switch } from "react-router-dom";
+import MainNavigation from "./components/layout/MainNavigation";
 import AllMeetupsPage from "./pages/AllMeetups";
 import FavoritesPage from "./pages/Favorites";
 import NewMeetupPage from "./pages/NewMeetup";
 
 function App() {
   return (
-    <Switch>
-      <Route path="/" exact>
-        <AllMeetupsPage />
-      </Route>
-      <Route path="/new-meetup">
-        <NewMeetupPage />
-      </Route>
-      <Route path="/favorites">
-        <FavoritesPage />
-      </Route>
-    </Switch>
+    <React.Fragment>
+      <MainNavigation />
+      <Switch>
+        <Route path="/" exact>
+          <AllMeetupsPage />
+        </Route>
+        <Route path="/new-meetup">
+          <NewMeetupPage />
+        </Route>
+        <Route path="/favorites">
+          <FavoritesPage />
+        </Route>
+      </Switch>
+    </React.Fragment>
   );
 }
 
