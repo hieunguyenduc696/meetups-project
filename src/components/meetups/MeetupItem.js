f2001c9e0e2469943bd0e6ca1abe82a3425b76af
Nguyen Duc Hieu <hieunguyenduc696@gmail.com>
2022-01-21 10:12:05 +0700

    fix some style

--- src/components/meetups/MeetupItem.js
+++ src/components/meetups/MeetupItem.js
@@ -8,13 +8,15 @@ const MeetupItem = (props) => {
         <div>
           <img src={props.image} alt={props.title} className={styles.image} />
         </div>
-        <div className={styles.info}>
-          <h2>{props.title}</h2>
-          <address>{props.address}</address>
-          <p>{props.description}</p>
-        </div>
-        <div className={styles.actions}>
-          <button className={styles.btn}>To Favorites</button>
+        <div className={styles.wrapper}>
+          <div className={styles.info}>
+            <h2>{props.title}</h2>
+            <address>{props.address}</address>
+            <p>{props.description}</p>
+          </div>
+          <div className={styles.actions}>
+            <button className={styles.btn}>To Favorites</button>
+          </div>
         </div>
       </Card>
     </li>
